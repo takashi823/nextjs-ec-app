2b228831e8a38ee1647a6a33ae18fbdd5ec78995
takashi823 <uchimura.takashi724@gmail.com>
2024-02-17 17:32:38 +0900

    add: APIから取得した商品情報を表示

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,12 +1,16 @@
-
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json([{
-    name: "胡麻鯖セット",
-    price: 5000,
-}, {
-    name: "明太子詰め合わせ",
-    price: 6000
-}]);
+  return NextResponse.json([
+    {
+      id: 1,
+      name: "胡麻鯖セット",
+      price: 5000,
+    },
+    {
+      id:2,
+      name: "明太子詰め合わせ",
+      price: 6000,
+    },
+  ]);
 }
